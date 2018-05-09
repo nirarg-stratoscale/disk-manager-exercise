ffd6aebb94be5d48afe87f8c89a686da93efac9d
Nir Argaman <nir.argaman@stratoscale.com>
2018-05-09 14:03:39 +0300

    Add storage.py - python tool to read disk info (lsblk). Current missing FreeCapacity information - consider to complete?

---
+++ lib/pytools/storage.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+import logging
+import os
+import subprocess32
+import json
+
+logging.basicConfig()
+_LOGGER = logging.getLogger(__name__)
+
+
+def _check_output_log_if_needed(*args, **kwargs):  # EXEMPT_FROM_CODE_COVERAGE
+    command = kwargs["args"] if "args" in kwargs else args[0]
+    _LOGGER.info(str(command))
+    output = subprocess32.check_output(*args,
+                                       stderr=subprocess32.STDOUT,
+                                       close_fds=True,
+                                       **kwargs)
+    _LOGGER.debug("Command '%(command)s' finished successfully. Output was:\n%(output)s",
+                  dict(command=command, output=output))
+    return output
+
+
+def _must_succeed(*args, **kwargs):  # EXEMPT_FROM_CODE_COVERAGE
+    try:
+        return _check_output_log_if_needed(*args, **kwargs)
+    except subprocess32.CalledProcessError as e:
+        _LOGGER.exception("Output was:\n%(output)s\nReturn code:%(returncode)d",
+                          dict(output=e.output, returncode=e.returncode))
+        raise
+
+
+def _get_devices_json():
+    cmd = ['lsblk', '-o', 'NAME,SERIAL,TYPE,MODEL,ROTA,UUID,SIZE', '-p', '-J', '-b']
+    return _must_succeed(cmd).strip()
+
+
+def _from_os_json_to_internal_json(os_json_data):
+    result_json_data = {}
+    block_devices_data = os_json_data['blockdevices']
+    result_json_data['disks'] = []
+    for element_data in block_devices_data:
+        if element_data['type'] == 'disk':
+            result_element_json_data = dict()
+            result_element_json_data['path'] = element_data['name']
+            result_element_json_data['serial'] = element_data['serial']
+            result_element_json_data['mediaType'] = 'SSD' if int(element_data['rota']) is 0 else 'HDD'
+            result_element_json_data['model'] = element_data['model']
+            #result_element_json_data['freeCapacityMB'] = '??????'
+            result_element_json_data['totalCapacityMB'] = (int(element_data['size']) / 1000000)
+            result_json_data['disks'].append(result_element_json_data)
+    return result_json_data
+
+
+def get_storage_list():
+    os_json = _get_devices_json()
+    os_json_data = json.loads(os_json)
+    result_json_data = _from_os_json_to_internal_json(os_json_data)
+    return result_json_data
+
+
+if __name__ == '__main__':  # EXEMPT_FROM_CODE_COVERAGE
+    try:
+        print get_storage_list()
+    except:
+        _LOGGER.exception('Failed with unknown except')
