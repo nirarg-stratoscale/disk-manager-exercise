f512bdaa25d332336356f44367663d805ab77c68
Nir Argaman <nir.argaman@stratoscale.com>
2018-05-13 09:22:43 +0300

    Change lsblk not to work with json

--- lib/pytools/storage.py
+++ lib/pytools/storage.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python
 import logging
 import subprocess
-import json
 
 logging.basicConfig()
 _LOGGER = logging.getLogger(__name__)
+
+
+def get_storage_list():
+    os_output = _get_devices_json()
+    result_json_data = _from_os_output_to_internal_json(os_output)
+    # os_json_data = json.loads(os_list)
+    # result_json_data = _from_os_json_to_internal_json(os_json_data)
+    return result_json_data
 
 
 def _check_output_log_if_needed(*args, **kwargs):  # EXEMPT_FROM_CODE_COVERAGE
@@ -29,31 +36,37 @@ def _must_succeed(*args, **kwargs):  # EXEMPT_FROM_CODE_COVERAGE
 
 
 def _get_devices_json():
-    cmd = ['lsblk', '-o', 'NAME,SERIAL,TYPE,MODEL,ROTA,UUID,SIZE', '-p', '-J', '-b']
+    cmd = ['lsblk', '-o', 'NAME,SERIAL,TYPE,ROTA,SIZE,MODEL', '-p', '-blnd', '-e', '4,7,10,11,43']
     return _must_succeed(cmd).strip()
 
 
-def _from_os_json_to_internal_json(os_json_data):
-    result_json_data = {}
-    block_devices_data = os_json_data['blockdevices']
+def _from_byte_to_string(byte_input):
+    result = byte_input.decode("utf-8")
+    return result
+
+
+def _from_os_output_element_to_internal_json(output_element):
+    result_element_json_data = dict()
+    result_element_json_data['path'] = _from_byte_to_string(output_element[0])
+    result_element_json_data['serial'] = _from_byte_to_string(output_element[1])
+    media_type = 'SSD' if int(_from_byte_to_string(output_element[3])) is 0 else 'HDD'
+    result_element_json_data['mediaType'] = media_type
+    model = str(_from_byte_to_string(output_element[5]))
+    if len(output_element) > 6:
+        for i in range(6, len(output_element)):
+            model = model + " " + _from_byte_to_string(output_element[i])
+    result_element_json_data['model'] = model
+    result_element_json_data['totalCapacityMB'] = int(int(output_element[4]) / 1000000)
+    return result_element_json_data
+
+
+def _from_os_output_to_internal_json(os_output):
+    result_json_data = dict()
     result_json_data['disks'] = []
-    for element_data in block_devices_data:
-        if element_data['type'] == 'disk':
-            result_element_json_data = dict()
-            result_element_json_data['path'] = element_data['name']
-            result_element_json_data['serial'] = element_data['serial']
-            media_type = 'SSD' if int(element_data['rota']) is 0 else 'HDD'
-            result_element_json_data['mediaType'] = media_type
-            result_element_json_data['model'] = element_data['model']
-            result_element_json_data['totalCapacityMB'] = int(int(element_data['size']) / 1000000)
-            result_json_data['disks'].append(result_element_json_data)
-    return result_json_data
-
-
-def get_storage_list():
-    os_json = _get_devices_json()
-    os_json_data = json.loads(os_json)
-    result_json_data = _from_os_json_to_internal_json(os_json_data)
+    for line in os_output.splitlines():
+        output_element = line.split()
+        element_internal_json = _from_os_output_element_to_internal_json(output_element)
+        result_json_data['disks'].append(element_internal_json)
     return result_json_data
 
 
