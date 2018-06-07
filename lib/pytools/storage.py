f28d98627e3fa1e45d4d1f6585e4cc2c4f03650a
Nir Argaman <nir.argaman@stratoscale.com>
2018-06-07 17:55:00 +0300

    STOR-2989: Add lib/pytools directory back + add mock storage.py file for subsystem tests

---
+++ lib/pytools/storage.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python
+import logging
+import subprocess
+import json
+
+logging.basicConfig()
+_LOGGER = logging.getLogger(__name__)
+
+HOST_PROC_DIR = "hostproc"
+MOUNT_COMMAND = "--mount=/" + HOST_PROC_DIR + "/1/ns/mnt"
+
+
+def get_storage_list():
+    os_output = _get_devices_storage()
+    result_json_data = _from_os_output_to_internal_json(os_output)
+    # os_json_data = json.loads(os_list)
+    # result_json_data = _from_os_json_to_internal_json(os_json_data)
+    return result_json_data
+
+
+def _check_output_log_if_needed(*args, **kwargs):  # EXEMPT_FROM_CODE_COVERAGE
+    command = kwargs["args"] if "args" in kwargs else args[0]
+    _LOGGER.info(str(command))
+    output = subprocess.check_output(*args,
+                                     stderr=subprocess.STDOUT,
+                                     close_fds=True,
+                                     **kwargs)
+    _LOGGER.debug("Command '%(command)s' finished successfully. Output was:\n%(output)s",
+                  dict(command=command, output=output))
+    return output
+
+
+def _must_succeed(*args, **kwargs):  # EXEMPT_FROM_CODE_COVERAGE
+    try:
+        return _check_output_log_if_needed(*args, **kwargs)
+    except subprocess.CalledProcessError as error1:
+        _LOGGER.exception("Output was:\n%(output)s\nReturn code:%(returncode)d",
+                          dict(output=error1.output, returncode=error1.returncode))
+        raise
+
+
+def _get_devices_storage():
+    cmd = ['nsenter', MOUNT_COMMAND, 'lsblk', '-o', 'NAME,SERIAL,TYPE,ROTA,SIZE,MODEL', '-p', '-blnd',
+           '-e', '4,7,10,11,43']
+    return _must_succeed(cmd).strip()
+
+
+def _from_byte_to_string(byte_input):
+    result = byte_input.decode("utf-8")
+    return result
+
+
+def _from_os_output_element_to_internal_json(output_element):
+    result_element_json_data = dict()
+    result_element_json_data['path'] = _from_byte_to_string(output_element[0])
+    result_element_json_data['serial'] = _from_byte_to_string(output_element[1])
+    media_type = 'SSD' if int(_from_byte_to_string(output_element[3])) is 0 else 'HDD'
+    result_element_json_data['mediaType'] = media_type
+    model = str(_from_byte_to_string(output_element[5]))
+    if len(output_element) > 6:
+        for i in range(6, len(output_element)):
+            model = model + " " + _from_byte_to_string(output_element[i])
+    result_element_json_data['model'] = model
+    result_element_json_data['totalCapacityMB'] = int(int(output_element[4]) / 1000000)
+    return result_element_json_data
+
+
+def _from_os_output_to_internal_json(os_output):
+    result_json_data = dict()
+    result_json_data['disks'] = []
+    for line in os_output.splitlines():
+        output_element = line.split()
+        element_internal_json = _from_os_output_element_to_internal_json(output_element)
+        result_json_data['disks'].append(element_internal_json)
+    return result_json_data['disks']
+
+
+if __name__ == '__main__':  # EXEMPT_FROM_CODE_COVERAGE
+    print(json.dumps(get_storage_list(), ensure_ascii=False))
---
+++ lib/pytools/tests/test_storage.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+import unittest
+import mock as mock
+import pytools.storage
+
+
+class TestStorage(unittest.TestCase):
+
+    @mock.patch('subprocess.check_output')
+    def test_storage_basic(self, check_output):
+        # Test one result, one SSD disk
+        check_output.return_value = b'/dev/sda S35NNY0HA05094 disk    0 512110190592 SAMSUNG MZ7TN512'
+        expected = {'disks': [{'mediaType': 'SSD', 'path': u'/dev/sda', 'serial': u'S35NNY0HA05094',
+                               'totalCapacityMB': 512110, 'model': u'SAMSUNG MZ7TN512'}]}
+        self.assertEqual(pytools.storage.get_storage_list(), expected)
+
+
+    @mock.patch('subprocess.check_output')
+    def test_torage_hdd(self, check_output):
+        # Test one result, one SSD disk
+        check_output.return_value = b'/dev/sda S35NNY0HA05094 disk    1 512110190592 SAMSUNG MZ7TN512'
+        expected = {'disks': [{'mediaType': 'HDD', 'path': u'/dev/sda', 'serial': u'S35NNY0HA05094',
+                               'totalCapacityMB': 512110, 'model': u'SAMSUNG MZ7TN512'}]}
+        self.assertEqual(pytools.storage.get_storage_list(), expected)
+
+
+    @mock.patch('subprocess.check_output')
+    def test_storage_model_one_word(self, check_output):
+        # Test one result, one SSD disk
+        check_output.return_value = b'/dev/sda S35NNY0HA05094 disk    1 512110190592 SAMSUNG'
+        expected = {'disks': [{'mediaType': 'HDD', 'path': u'/dev/sda', 'serial': u'S35NNY0HA05094',
+                               'totalCapacityMB': 512110, 'model': u'SAMSUNG'}]}
+        self.assertEqual(pytools.storage.get_storage_list(), expected)
+
+
+    @mock.patch('subprocess.check_output')
+    def test_storage_model_three_words(self, check_output):
+        # Test one result, one SSD disk
+        check_output.return_value = b'/dev/sda S35NNY0HA05094 disk    1 512110190592 SAMSUNG MZ7TN512 ASDF'
+        expected = {'disks': [{'mediaType': 'HDD', 'path': u'/dev/sda', 'serial': u'S35NNY0HA05094',
+                               'totalCapacityMB': 512110, 'model': u'SAMSUNG MZ7TN512 ASDF'}]}
+        self.assertEqual(pytools.storage.get_storage_list(), expected)
+
+
+    @mock.patch('subprocess.check_output')
+    def test_storage_model_special(self, check_output):
+        # Test one result, one SSD disk
+        check_output.return_value = b'/dev/sda S35NNY0HA05094 disk    1 512110190592 !@#$%'
+        expected = {'disks': [{'mediaType': 'HDD', 'path': u'/dev/sda', 'serial': u'S35NNY0HA05094',
+                               'totalCapacityMB': 512110, 'model': u'!@#$%'}]}
+        self.assertEqual(pytools.storage.get_storage_list(), expected)
+
+
+    @mock.patch('subprocess.check_output')
+    def test_storage_model_empty(self, check_output):
+        # Test one result, one SSD disk
+        check_output.return_value = b''
+        expected = {'disks': []}
+        self.assertEqual(pytools.storage.get_storage_list(), expected)
+
+
+    @mock.patch('subprocess.check_output')
+    def test_storage_model_two(self, check_output):
+        # Test one result, one SSD disk
+        check_output.return_value = b'/dev/sda1 S35NNY0HA0509411 disk 1 10000000 ASDFGH\n' \
+                                    b'/dev/sda2 S35NNY0HA0509422 disk    0 512110190592 QWERTY'
+        expected = {'disks': [{'mediaType': 'HDD', 'path': u'/dev/sda1',
+                               'serial': u'S35NNY0HA0509411', 'totalCapacityMB': 10, 'model': u'ASDFGH'},
+                              {'mediaType': 'SSD', 'path': u'/dev/sda2', 'serial': u'S35NNY0HA0509422',
+                               'totalCapacityMB': 512110, 'model': u'QWERTY'}]}
+        self.assertEqual(pytools.storage.get_storage_list(), expected)
